8a86d4016b34a1e2cc68686a893e0da8d7edc292
Aphinan Peerachaidacho <aphinan.pee@gmail.com>
2020-06-28 14:45:55 +0700

    - Fix style - Add award new layout

--- resources/views/award/index.blade.php
+++ resources/views/award/index.blade.php
@@ -3,33 +3,23 @@
 @section('title', 'Award')
 
 @section('content')
-    <div class="sub-title">
-        <div class="container">
-            <h1 class="text-ellipsis text-center">
-                {{ __('messages.award.awardHeading') }}
-            </h1>
+<div class="sub-title">
+    <div class="container">
+        <h1 class="text-ellipsis text-center">
+            {{ __('messages.award.awardHeading') }}
+        </h1>
+    </div>
+</div>
+<section class="bg-white">
+    <div class="container">
+        <div id="columns">
+        @foreach($awards as $award)
+        <figure>
+            <img src="{{ url('image/show/'.$award->image_id) }}"
+                alt="Card image cap">
+        </figure>
+        @endforeach
         </div>
     </div>
-    <section class="bg-white">
-        <div class="container">
-            <div class="row">
-                @foreach($awards as $award)
-                    <div class="col-md-6 col-lg-4">
-                        <div class="card" onclick="location.href='{{ url('awards/'.$award->id) }}'">
-                            <div class="image" style="height: 100% !important;">
-                                <img class="award-img-wrapper card-img-top"
-                                src="{{ url('image/show/'.$award->image_id) }}"
-                                alt="Card image cap">
-                            </div>
-                            <div class="card-body">
-                                <h5 class="card-title text-center">
-                                    {!! $award->name !!}
-                                </h5>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        </div>
-    </section>
+</section>
 @endsection
