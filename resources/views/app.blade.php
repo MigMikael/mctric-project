acbcf3c2d16b72eaaba810d40b38287139c65889
Aphinan Peerachaidacho <aphinan.pee@gmail.com>
2020-07-09 01:01:22 +0700

    - Fix slide responsive

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -152,11 +152,11 @@ $(document).ready(function(){
         responsiveClass:true,
         responsive: {
             0: {
-                items: 2,
+                items: 1,
                 nav:true
                 },
                 400: {
-                items: 2,
+                items: 1,
                 nav:false
                 },
                 600:{
