92072c65f814377a16b48a229ebe6ecd31f4d710
Aphinan Peerachaidacho <aphinan.pee@gmail.com>
2024-10-15 18:57:45 +0700

    Fix: search filter in business list

--- resources/views/business/list.blade.php
+++ resources/views/business/list.blade.php
@@ -30,12 +30,19 @@
                         {!! Form::open([
                             'method' => 'get',
                             'url' => '/dashboard/businesses/status/' . $status,
-                            'class' => 'form-inline',
+                            'class' => '',
                         ]) !!}
-                        <div class="form-group form-filter" style="width: 100%">
-                            {!! Form::label('Filter By Year:') !!}
-                            {!! Form::select('year', $years, $year, ['class' => 'form-control select-filter-date']) !!}
-                            <button class="btn btn-primary" type="submit">{{ __('messages.app.searchButton') }}</button>
+                        <div class="row">
+                            <div class="col-md-5 col-lg-6 form-group">
+                                <h5 style="font-weight: 400; margin-top: 7px; margin-bottom: 0px;">Project Result: {{ $businesses->total() }}</h5>
+                            </div>
+                            <div class="col-md-4 col-lg-4 form-group">
+                                {!! Form::select('year', $years, $year, ['class' => 'form-control select-filter-date']) !!}
+                            </div>
+                            <div class="col-md-3 col-lg-2 form-group">
+                                <button class="btn btn-outline-primary btn-block"
+                                    type="submit">{{ __('messages.app.searchButton') }}</button>
+                            </div>
                         </div>
                         {!! Form::close() !!}
                     </div>
@@ -46,31 +53,43 @@
                         {!! Form::open([
                             'method' => 'get',
                             'url' => '/businesses/category/' . $category->slug,
-                            'class' => 'form-inline',
+                            'class' => '',
                         ]) !!}
-                        <div class="form-group form-filter" style="width: 100%">
-                            {!! Form::label('Filter By Year:') !!}
-                            {!! Form::select('year', $years, $year, ['class' => 'form-control select-filter-date']) !!}
-                            <button class="btn btn-primary" type="submit">{{ __('messages.app.searchButton') }}</button>
+                        <div class="row">
+                            <div class="col-md-5 col-lg-6 form-group">
+                                <h5 style="font-weight: 400; margin-top: 7px; margin-bottom: 0px;">Project Result: {{ $businesses->total() }}</h5>
+                            </div>
+                            <div class="col-md-4 col-lg-4 form-group">
+                                {!! Form::select('year', $years, $year, ['class' => 'form-control select-filter-date']) !!}
+                            </div>
+                            <div class="col-md-3 col-lg-2 form-group">
+                                <button class="btn btn-outline-primary btn-block"
+                                    type="submit">{{ __('messages.app.searchButton') }}</button>
+                            </div>
                         </div>
                         {!! Form::close() !!}
                     </div>
                 </div>
             @endif
-            <div class="row">
+
+            <div class="row mb-3">
                 @foreach ($businesses as $business)
                     <div class="col-md-6 col-lg-4">
                         <div class="card" onclick="location.href='{{ url('businesses/' . $business->id) }}'">
                             <img class="card-img-top" src="{{ url('image/show/' . $business->cover_image) }}"
                                 alt="Card image cap">
-                            <div class="card-body" style="
-                            @if (Request::is('dashboard/businesses/status/*'))
-                            height: 130px;
+                            <div class="card-body"
+                                style="
+                            @if (Request::is('dashboard/businesses/status/*')) height: 180px;
                             @else
-                            height: 100px;
+                            height: 130px;
                             @endif
                             ">
                                 <h5 class="card-title text-center">{{ $business->name }}</h5>
+                                <div class="text-center">
+                                    <span style="font-size: 14px;">{{ date('F Y', strtotime($business->contract_start)) }} -
+                                    {{ date('F Y', strtotime($business->contract_end)) }}</span>
+                                </div>
                                 @if (Request::is('dashboard/businesses/status/*'))
                                     <div class="progress" style="margin-top: 25px; margin-bottom: 10px;">
                                         <div class="progress-bar" role="progressbar"
@@ -82,21 +101,22 @@
                             </div>
                             @if (Request::is('dashboard/businesses/status/*'))
                                 <div class="card-footer text-center">
-                                    <a class="btn btn-warning mr-3" href="{{ url('businesses/' . $business->id . '/edit/') }}">
+                                    <a class="btn btn-warning mr-3"
+                                        href="{{ url('businesses/' . $business->id . '/edit/') }}">
                                         {{ __('messages.app.editButton') }}</a>
-                                        <!--
-                                    {!! Form::model($business, ['method' => 'delete', 'url' => '/businesses/' . $business->id]) !!}
-                                    <button class="btn btn-danger"
-                                        type="submit">{{ __('messages.app.deleteButton') }}</button>
-                                    {!! Form::close() !!}
-                                    -->
+                                    <!--
+                                        {!! Form::model($business, ['method' => 'delete', 'url' => '/businesses/' . $business->id]) !!}
+                                        <button class="btn btn-danger"
+                                            type="submit">{{ __('messages.app.deleteButton') }}</button>
+                                        {!! Form::close() !!}
+                                        -->
                                 </div>
                             @endif
                         </div>
                     </div>
                 @endforeach
             </div>
-            <div class="row" style="justify-content: center">
+            <div class="row" style="justify-content: center;">
                 <!--{{ $businesses->links() }}-->
                 {{ $businesses->appends(['year' => $year])->links() }}
             </div>
