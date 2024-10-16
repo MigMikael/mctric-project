d6f5c451bb947620743aacb2a8f813eb697d4a2f
Aphinan Peerachaidacho <aphinan.pee@gmail.com>
2024-10-16 20:39:51 +0700

    Fix: career detail

--- resources/views/career/show.blade.php
+++ resources/views/career/show.blade.php
@@ -3,7 +3,8 @@
 @section('title', __('messages.home.webTitle'))
 
 @section('content')
-    <div class="sub-title" style="background-image: url({{ url('image/show/'.$career->image_id) }}) , linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)) !important;
+    <div class="sub-title"
+        style="background-image: url({{ url('image/show/' . $career->image_id) }}) , linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)) !important;
         background-blend-mode: overlay;">
         <div class="container">
             <h1 class="text-ellipsis text-center">
@@ -15,7 +16,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="card-content">
+                    <div class="card-content" style="padding-top: 15px; padding-bottom: 25px;">
                         <div class="card-body">
                             <h5>{{ __('messages.career.description') }}</h5>
                             <p>{!! $career->description !!}</p>
@@ -23,12 +24,12 @@
                             <h5>{{ __('messages.career.qualification') }}</h5>
                             <p>{!! $career->qualification !!}</p>
                         </div>
-                        <div class="card-footer text-center" style="background-color: #FFFFFF !important;
-                        border-top: unset !important;">
-                            <button type="button" class="btn learn-more-btn"
-                            onclick="location.href='https://docs.google.com/forms/d/e/1FAIpQLScLzMQHHkAObKzT2A-SQ2L4QFRKwMB7gcjc13gP1Wb9C5IBeA/viewform'">
-                            {{ __('messages.career.applyBtn') }}
-                        </button>
+                        <div class="card-footer text-center"
+                            style="background-color: #FFFFFF !important; border-top: unset !important;">
+                            <a href="https://docs.google.com/forms/d/e/1FAIpQLScLzMQHHkAObKzT2A-SQ2L4QFRKwMB7gcjc13gP1Wb9C5IBeA/viewform"
+                                class="btn learn-more-btn">
+                                {{ __('messages.career.applyBtn') }}
+                            </a>
                         </div>
                     </div>
                 </div>
