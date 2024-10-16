d2de59ef6005d313fd3a7a3458e6190aca2f8a31
Aphinan Peerachaidacho <aphinan.pee@gmail.com>
2024-10-16 20:32:27 +0700

    Fix: card hyperlink

--- resources/views/award/index.blade.php
+++ resources/views/award/index.blade.php
@@ -3,32 +3,34 @@
 @section('title', __('messages.home.webTitle'))
 
 @section('content')
-<header class="sub-title">
-    <div class="sub-title-overlay"></div>
-    <div class="container h-100">
-        <div class="d-flex h-100 text-center align-items-center">
-            <div class="w-100 text-white" style="z-index: 3">
-                <h1 class="text-ellipsis text-center">
-                    {{ __('messages.award.awardHeading') }}
-                </h1>
-            </div>
-        </div>
-    </div>
-</header>
-<section class="services bg-white">
-    <div class="container">
-        <div class="row">
-            @foreach($awards as $award)
-            <div class="col-md-4 col-lg-4">
-                <div class="service-box" onclick="location.href='{{ url('awards/'.$award->id) }}'"
-                    style="background-image: url(https://images.pexels.com/photos/310479/pexels-photo-310479.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940), linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3));">
-                    <div class="services-wrapper">
-                        <h4>{{ $award->name }}</h4>
-                    </div>
+    <header class="sub-title">
+        <div class="sub-title-overlay"></div>
+        <div class="container h-100">
+            <div class="d-flex h-100 text-center align-items-center">
+                <div class="w-100 text-white" style="z-index: 3">
+                    <h1 class="text-ellipsis text-center">
+                        {{ __('messages.award.awardHeading') }}
+                    </h1>
                 </div>
             </div>
-            @endforeach
         </div>
-    </div>
-</section>
+    </header>
+    <section class="services bg-white">
+        <div class="container">
+            <div class="row">
+                @foreach ($awards as $award)
+                    <div class="col-md-4 col-lg-4">
+                        <a href="{{ url('awards/' . $award->id) }}">
+                            <div class="service-box"
+                                style="background-image: url(https://images.pexels.com/photos/310479/pexels-photo-310479.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940), linear-gradient(rgba(0,0,0,0.3),rgba(0,0,0,0.3));">
+                                <div class="services-wrapper">
+                                    <h4>{{ $award->name }}</h4>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/career/index.blade.php
+++ resources/views/career/index.blade.php
@@ -3,34 +3,36 @@
 @section('title', __('messages.home.webTitle'))
 
 @section('content')
-<header class="sub-title">
-    <div class="sub-title-overlay"></div>
-    <div class="container h-100">
-        <div class="d-flex h-100 text-center align-items-center">
-            <div class="w-100 text-white" style="z-index: 3">
-                <h1 class="text-ellipsis text-center">
-                    {{ __('messages.career.careerHeading') }}
-                </h1>
-            </div>
-        </div>
-    </div>
-</header>
-<section class="bg-white">
-    <div class="container">
-        <div class="row">
-            @foreach($careers as $career)
-            <div class="col-md-6 col-lg-4">
-                <div class="card" onclick="location.href='{{ url('careers/'.$career->id) }}'">
-                    <img class="card-img-top" src="{{ url('image/show/'.$career->image_id) }}" alt="">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">
-                            {!! $career->name !!}
-                        </h5>
-                    </div>
+    <header class="sub-title">
+        <div class="sub-title-overlay"></div>
+        <div class="container h-100">
+            <div class="d-flex h-100 text-center align-items-center">
+                <div class="w-100 text-white" style="z-index: 3">
+                    <h1 class="text-ellipsis text-center">
+                        {{ __('messages.career.careerHeading') }}
+                    </h1>
                 </div>
             </div>
-            @endforeach
         </div>
-    </div>
-</section>
+    </header>
+    <section class="bg-white">
+        <div class="container">
+            <div class="row">
+                @foreach ($careers as $career)
+                    <div class="col-md-6 col-lg-4">
+                        <a href="{{ url('careers/' . $career->id) }}" style="text-decoration: none !important;">
+                            <div class="card">
+                                <img class="card-img-top" src="{{ url('image/show/' . $career->image_id) }}" alt="">
+                                <div class="card-body">
+                                    <h5 class="card-title text-center" style="color: #000;">
+                                        {!! $career->name !!}
+                                    </h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
 @endsection
