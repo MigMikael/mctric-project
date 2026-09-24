450ec0c26739a1eb23d07790cfe40fc4abf6a755
Aphinan Peerachaidacho <aphinan.pee@gmail.com>
2026-09-24 21:47:51 +0700

    Fix: show client

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -19,6 +19,7 @@ class ClientsController extends Controller
     {
         $clients = Clients::where('display', true)
                 ->orderBy('created_at', 'asc')
+                ->take(32)
                 ->get();
         return view('client.index', ['clients' => $clients]);
     }
